6a53bfce9cf521c86ceefa28e4961de3f9909ad3
Arian Zargaran <arianzf@gmail.com>
2020-10-08 22:59:08 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
+
+// https://github.com/DavidArmendariz/react-testing-course
+// https://www.udemy.com/course/testing-react-apps-with-react-testing-library-rtl/
