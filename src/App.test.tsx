2be963c17ecb6e2530a0933f07e961157b44985c
Arian Zargaran <arian.zargaran@cabify.com>
2020-10-10 23:08:14 +0200

    First tests in RTL

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('When the App is compiled', ()=> {
+  test('should render the App component', () => {
+    // render method tests the snipet of HTML involved in the test
+    render(
+      <App />
+    );
+    // outputs the snipet of HTML involved in the test
+    screen.debug();
+  });
+
+  test('should render an input label with the text "Input"', () => {
+    render(<App />);
+    screen.getByText(/Input/);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><CustomInput value={text} onChange={handleChange}>Input:</CustomInput>
+      <p>You typed: {text ?? '...'}</p>
     </div>
   );
+};
+
+interface CustomInputProps {
+  children: React.ReactNode,
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  value: string
+}
+
+export const CustomInput = ({ children, value, onChange }: CustomInputProps) => {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" value={value} onChange={onChange} />
+    </div>
+  );
+
 }
 
 export default App;
